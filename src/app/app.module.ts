dac727135930d06a9856cf9f410da9ac3b483403
Antonio Busa <antoniobusa@antonios-mbp.lan>
2018-03-13 10:25:45 -0600

    Assignment 1 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { MenuComponent } from './menu/menu.component';
-import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent,
+    DishdetailComponent,
   ],
   imports: [
     BrowserModule,
