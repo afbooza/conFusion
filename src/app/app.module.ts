e349236f015a8c40fd567f6298fc0b79e158c5b0
Antonio Busa <antoniobusa@antonios-mbp.lan>
2018-03-24 18:57:22 -0600

    Header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,17 @@ import { environment } from '../environments/environment';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
-import { DishService } from './services/dish.service';  
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';  
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
