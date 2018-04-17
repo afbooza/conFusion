bc5e7c99f6ad19db6441b99da03e526d4612f18f
Antonio Busa <antoniobusa@Antonios-MBP.home>
2018-04-17 18:23:46 -0400

    single app part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
 import 'hammerjs'; 
-import { ServiceWorkerModule } from '@angular/service-worker';
+
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';  
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';  
+
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module'
 
@@ -41,7 +43,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
