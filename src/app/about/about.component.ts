71e0417a19ae9034cf9b125f9140fdb640ac6278
Antonio Busa <antoniobusa@Antonios-MBP.home>
2018-04-19 20:07:05 -0400

    Assignment 2 ANGULAR MATERIAL is not working for about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Leader } from '../shared/leader';
+
+
+import { LeaderService } from '../services/leader.service';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  leaders: Leader[];
+  selectedLeader: Leader;
+
+  constructor( private leaderservice: LeaderService) { }
 
   ngOnInit() {
+    this.leaders = this.leaderservice.getLeaders();
   }
 
 }
